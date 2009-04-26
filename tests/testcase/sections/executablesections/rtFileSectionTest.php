2e850dbf5692889f257a779bd66e6f6f63515bec
Zoe Slattery <zoe@php.net>
2009-04-26 07:12:58 +0000

    tidy

--- tests/testcase/sections/executablesections/rtFileSectionTest.php
+++ tests/testcase/sections/executablesections/rtFileSectionTest.php
@@ -3,12 +3,12 @@
 require_once 'PHPUnit/Framework.php';
 require_once dirname(__FILE__) . '../../../../../src/rtAutoload.php';
 
-class rtSkipIfSectionTest extends PHPUnit_Framework_TestCase
+class rtFileSectionTest extends PHPUnit_Framework_TestCase
 {
     public function testCreateInstance()
     {
-        $skipifSection = new rtSkipIfSection('SKIPIF', array('<?php', 'echo "hello world";', '?>')); 
-        $code = $skipifSection->getContents();
+        $fileSection = new rtFileSection('FILE', array('<?php', 'echo "hello world";', '?>')); 
+        $code = $fileSection->getContents();
 
         $this->assertEquals('<?php', $code[0]);
     }
--- tests/testcase/sections/executablesections/rtSkipIfSectionTest.php
+++ tests/testcase/sections/executablesections/rtSkipIfSectionTest.php
@@ -3,12 +3,12 @@
 require_once 'PHPUnit/Framework.php';
 require_once dirname(__FILE__) . '../../../../../src/rtAutoload.php';
 
-class rtFileSectionTest extends PHPUnit_Framework_TestCase
+class rtSkipifSectionTest extends PHPUnit_Framework_TestCase
 {
     public function testCreateInstance()
     {
-        $fileSection = new rtFileSection('FILE', array('<?php', 'echo "hello world";', '?>')); 
-        $code = $fileSection->getContents();
+        $skipifSection = new rtSkipIfSection('SKIPIF', array('<?php', 'echo "hello world";', '?>')); 
+        $code = $skipifSection->getContents();
 
         $this->assertEquals('<?php', $code[0]);
     }
