4d0e02564f6f7b2951fde29d1b93158ccb70bfb7
Zoe Slattery <zoe@php.net>
2009-04-29 18:51:33 +0000

    gzip post section class

---
+++ src/testcase/sections/configurationsections/rtGzipPostSection.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * rtPostSection
+ * Sets environment variables for GZIP_POST section
+ *
+ *
+ * @category  Testing
+ * @package   RUNTESTS
+ * @author    Zoe Slattery <zoe@php.net>
+ * @author    Stefan Priebsch <spriebsch@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.php.net/license/3_01.txt PHP License 3.01
+ * @link      http://qa.php.net/
+ */
+class rtPostSection extends rtConfigurationSection
+{
+    private $postVariables = array();
+    private $postFileName;
+
+    protected function init()
+    { 
+        $postString = implode('\n', $this->sectionContents);
+        $gzipPostString = gzencode($postString);
+        $this->postVariables['CONTENT_TYPE'] = 'application/x-www-form-urlencoded';
+        $this->postVariables['CONTENT_LENGTH'] = strlen($gzipPostString);
+        $this->postVariables['REQUEST_METHOD'] = 'POST';
+
+        $this->postFileName = tempnam(sys_get_temp_dir(), 'post');
+        
+        file_put_contents($this->postFileName, $postString);
+    }
+
+    /**
+     * Additional GET environment variables required by the test
+     *
+     * @return array
+     */
+    public function getPostVariables()
+    {
+        return $this->postVariables;
+    }
+
+    public function getPostFileName()
+    {
+        return $this->postFileName;
+    }
+}
+?>
