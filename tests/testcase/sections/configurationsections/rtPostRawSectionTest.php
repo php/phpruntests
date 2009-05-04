64fefb318e4152a121055d7001ea6df814b776bf
Zoe Slattery <zoe@php.net>
2009-05-04 15:45:54 +0000

    test for post raw section

---
+++ tests/testcase/sections/configurationsections/rtPostRawSectionTest.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+require_once dirname(__FILE__) . '../../../../../src/rtAutoload.php';
+
+class rtPostRawSectionTest extends PHPUnit_Framework_TestCase
+{
+    public function testCreateInstance() 
+    {
+        $post_array = array( 'fred',
+                             'Content-Type:the first',
+                             'joe',
+                             'Content-Type:the second',
+                             'mary',
+                            );
+        $postSection = new rtPostRawSection('POST', $post_array);  
+        
+        $envVars = $postSection->getPostVariables();
+        $this->assertEquals('POST', $envVars['REQUEST_METHOD']);
+        $this->assertEquals('the first',$envVars['CONTENT_TYPE']);
+        
+        $fileName = $postSection->getPostFileName();
+        $string = file_get_contents($fileName);        
+        $this->assertEquals('fred\njoe\nContent-Type:the second\nmary', $string);
+    }
+}
+?>
