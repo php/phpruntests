755d0b31cf0b9e86efbc9ba3d06d1126fd5dc6ae
Georg Gradwohl <g2@php.net>
2009-05-12 16:05:00 +0000

    phpruntests: improved version-check in the start-script

--- src/run-tests.php
+++ src/run-tests.php
@@ -8,34 +8,13 @@
  * check the version of the running php-executable and
  * ensure that is 5.3 or higher
  */
-$v = phpversion();
-
-$major = substr($v, 0, 1);
-$minor = substr($v, 2, 1);
-
-$isVersionOk = false;
-
-if ($major > 5) {
-    $isVersionOk = true;
-
-} elseif ($major == 5) {
-
-    if ($minor >= 3) {
-        $isVersionOk = true;
-    }
+if (version_compare(PHP_VERSION, '5.3.0RC1', '<')) {
+    die('This version of run-tests requires PHP 5.3RC1 or higher, you are running ' . PHP_VERSION . "\n");
 }
 
+require_once dirname(__FILE__) . '/rtAutoload.php';
 
-if ($isVersionOk) {
-
-    require_once dirname(__FILE__) . '/rtAutoload.php';
-
-    $phpTestRun = new rtPhpTestRun($argv);
-    $phpTestRun->run();
-
-} else {
-	
-    die("This version of run-tests requires PHP 5.3 or higher.\nYou can check your current version by executing 'php -v' from the command line.\n");
-}
+$phpTestRun = new rtPhpTestRun($argv);
+$phpTestRun->run();
 
 ?>
