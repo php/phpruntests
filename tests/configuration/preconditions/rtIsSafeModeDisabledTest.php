f1cfcc8737bb886fc52506ef3617b6dd8d43f012
Stefan Priebsch <spriebsch@php.net>
2009-05-12 22:56:22 +0000

    Fixed test to work with refactored rtRuntestsConfiguration.

--- tests/configuration/preconditions/rtIsSafeModeDisabledTest.php
+++ tests/configuration/preconditions/rtIsSafeModeDisabledTest.php
@@ -1,24 +1,52 @@
 <?php
+/**
+ * rtIsSafeModeDisabledTest
+ *
+ * @category  Testing
+ * @package   RUNTESTS
+ * @author    Zoe Slattery <zoe@php.net>
+ * @author    Stefan Priebsch <spriebsch@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.php.net/license/3_01.txt PHP License 3.01
+ * @link      http://qa.php.net/
+ */
 
 require_once 'PHPUnit/Framework.php';
 require_once dirname(__FILE__) . '../../../../src/rtAutoload.php';
 
+/**
+ * Tests for rtIsSafeModeDisabledTest precondition.
+ *
+ * @category  Testing
+ * @package   RUNTESTS
+ * @author    Zoe Slattery <zoe@php.net>
+ * @author    Stefan Priebsch <spriebsch@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.php.net/license/3_01.txt PHP License 3.01
+ * @link      http://qa.php.net/
+ */
 class rtIssafeModeDisabledTest extends PHPUnit_Framework_TestCase
 {
+    protected function setUp()
+    {
+        $this->preCondition = new rtIsSafeModeDisabled();
+    }
+
+    protected function tearDown()
+    {
+        unset($this->preCondition);
+    }
+
     public function testDisabled()
     {
-        $pre = new rtIsSafeModeDisabled();
+        $runtestsConfiguration = rtRuntestsConfiguration::getInstance(array());
 
-        $this->assertTrue($pre->check());
+        $this->assertTrue($this->preCondition->check($runtestsConfiguration));
     }
 
-    public function testgetMessage()
+    public function testGetMessage()
     {
-        $pre = new rtIsSafeModeDisabled();
-
-        $this->assertEquals($pre->getMessage('safeModeNotDisabled'), rtText::get('safeModeNotDisabled'));
+        $this->assertEquals($this->preCondition->getMessage('safeModeNotDisabled'), rtText::get('safeModeNotDisabled'));
     }
-    
-    //Not sure how to check if it is missing?
 }
 ?>
