35992b774c687c9b787ef828eff6228c11fad82e
Zoe Slattery <zoe@php.net>
2009-05-12 20:16:05 +0000

    fixing after stephans name changes

--- tests/configuration/rtRuntestsConfigurationTest.php
+++ tests/configuration/rtRuntestsConfigurationTest.php
@@ -5,25 +5,29 @@ require_once dirname(__FILE__) . '../../../src/rtAutoload.php';
 
 class rtRuntestsConfigurationTest extends PHPUnit_Framework_TestCase
 {
+    
+    public function setUp() {
+        $this->php = trim(shell_exec("which php"));
+    }
     public function testCreateUnix()
     {
-        $config = rtRuntestsConfiguration::getInstance(array('run-tests.php', '-p', 'a-php-exe', 'test.phpt'));
+        $config = rtRuntestsConfiguration::getInstance(array('run-tests.php', '-p', $this->php, 'test.phpt'));
         $config->configure();
         
-        $testFiles = $config->getSetting('TestFiles');
+        $testFiles = $config->getSetting('rtTestFileSetting');
 
-        $this->assertEquals('a-php-exe', $config->getSetting('PhpExecutable'));
+        $this->assertEquals('/usr/local/bin/php', $config->getSetting('rtPhpExecutableSetting'));
         $this->assertEquals('test.phpt', $testFiles[0]);
     }  
 
     public function testCreateWin()
     {
-        $config = rtRuntestsConfiguration::getInstance(array('run-tests.php', '-p', 'a-php-exe', 'test.phpt'), 'Windows');
+        $config = rtRuntestsConfiguration::getInstance(array('run-tests.php', '-p', $this->php, 'test.phpt'), 'Windows');
         $config->configure();
         
-        $testFiles = $config->getSetting('TestFiles');
+        $testFiles = $config->getSetting('rtTestFileSetting');
 
-        $this->assertEquals('a-php-exe', $config->getSetting('PhpExecutable'));
+        $this->assertEquals('/usr/local/bin/php', $config->getSetting('rtPhpExecutableSetting'));
         $this->assertEquals('test.phpt', $testFiles[0]);
     } 
 }
