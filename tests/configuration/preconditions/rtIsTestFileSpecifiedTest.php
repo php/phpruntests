705341af37612e9767c40dd4c28baa1fddc98a12
Stefan Priebsch <spriebsch@php.net>
2009-05-12 23:20:28 +0000

    Fixed test to work with refactored rtRuntestsConfiguration.

--- tests/configuration/preconditions/rtIsTestFileSpecifiedTest.php
+++ tests/configuration/preconditions/rtIsTestFileSpecifiedTest.php
@@ -1,69 +1,81 @@
 <?php
+/**
+ * rtIsTestFileSpecifiedTest
+ *
+ * @category  Testing
+ * @package   RUNTESTS
+ * @author    Zoe Slattery <zoe@php.net>
+ * @author    Stefan Priebsch <spriebsch@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.php.net/license/3_01.txt PHP License 3.01
+ * @link      http://qa.php.net/
+ */
 
 require_once 'PHPUnit/Framework.php';
 require_once dirname(__FILE__) . '../../../../src/rtAutoload.php';
 
+/**
+ * Tests for rtIsTestFileSpecifiedTest precondition.
+ *
+ * @category  Testing
+ * @package   RUNTESTS
+ * @author    Zoe Slattery <zoe@php.net>
+ * @author    Stefan Priebsch <spriebsch@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.php.net/license/3_01.txt PHP License 3.01
+ * @link      http://qa.php.net/
+ */
 class rtIsTestFileSpecifiedTest extends PHPUnit_Framework_TestCase
 {
+    protected function setUp()
+    {
+        $this->preCondition = new rtIsTestFileSpecified();
+    }
+
+    protected function tearDown()
+    {
+        unset($this->preCondition);
+    }
+
     public function testClOptionR()
     {
-        $env = rtEnvironmentVariables::getInstance();
-        $clo = new rtCommandLineOptions();
-        $clo->parse(array('run-tests.php', '-r', 'some-file'));
+        $runtestsConfiguration = rtRuntestsConfiguration::getInstance(array('run-tests.php', '-r', 'some-file'));
 
-        $pre = new rtIsTestFileSpecified();
-
-        $this->assertTrue($pre->check($clo, $env));
+        $this->assertTrue($this->preCondition->check($runtestsConfiguration));
     }
 
     public function testCLOptionL()
     {
-        $env = rtEnvironmentVariables::getInstance();
-        $clo = new rtCommandLineOptions();
-        $clo->parse(array('run-tests.php', '-l', 'some-file'));
+        $runtestsConfiguration = rtRuntestsConfiguration::getInstance(array('run-tests.php', '-l', 'some-file'));
 
-        $pre = new rtIsTestFileSpecified();
-
-        $this->assertTrue($pre->check($clo, $env));
+        $this->assertTrue($this->preCondition->check($runtestsConfiguration));
     }
 
     public function testCLOptionFileName()
     {
-        $env = rtEnvironmentVariables::getInstance();
-        $clo = new rtCommandLineOptions();
-        $clo->parse(array('run-tests.php', 'some-test-file'));
+        $runtestsConfiguration = rtRuntestsConfiguration::getInstance(array('run-tests.php', 'some-test-file'));
 
-        $pre = new rtIsTestFileSpecified();
-
-        $this->assertTrue($pre->check($clo, $env));
+        $this->assertTrue($this->preCondition->check($runtestsConfiguration));
     }
 
     public function testEnvVar()
     {
-        $env = rtEnvironmentVariables::getInstance();
-        $clo = new rtCommandLineOptions();
-        $env->setvariable('TEST_PHP_USER', 'some-file');
+        $runtestsConfiguration = rtRuntestsConfiguration::getInstance(array());
+        $runtestsConfiguration->setEnvironmentVariable('TEST_PHP_USER', 'some-file');
 
-        $pre = new rtIsTestFileSpecified();
-
-        $this->assertTrue($pre->check($clo, $env));
+        $this->assertTrue($this->preCondition->check($runtestsConfiguration));
     }
 
     public function testNotSpecified()
     {
-        $env = rtEnvironmentVariables::getInstance();
-        $clo = new rtCommandLineOptions();
+        $runtestsConfiguration = rtRuntestsConfiguration::getInstance(array());
 
-        $pre = new rtIsTestFileSpecified();
-
-        $this->assertFalse($pre->check($clo, $env));
+        $this->assertFalse($this->preCondition->check($runtestsConfiguration));
     }
 
-    public function testGetText()
+    public function testGetMessage()
     {
-        $pre = new rtIsTestFileSpecified();
-
-        $this->assertEquals($pre->getMessage('missingTestFile'), rtText::get('missingTestFile'));
+        $this->assertEquals($this->preCondition->getMessage('missingTestFile'), rtText::get('missingTestFile'));
     }
 }
 ?>
