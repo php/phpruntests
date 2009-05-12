bcba3e28caf525e1b1de525fbf89b62b89bb4616
Stefan Priebsch <spriebsch@php.net>
2009-05-12 22:40:32 +0000

    Added comments. Renamed test method for better readability.

--- tests/configuration/preconditions/rtIsExecutableSetTest.php
+++ tests/configuration/preconditions/rtIsExecutableSetTest.php
@@ -1,8 +1,30 @@
 <?php
+/**
+ * rtIsExecutableSetTest
+ *
+ * @category  Testing
+ * @package   RUNTESTS
+ * @author    Zoe Slattery <zoe@php.net>
+ * @author    Stefan Priebsch <spriebsch@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.php.net/license/3_01.txt PHP License 3.01
+ * @link      http://qa.php.net/
+ */
 
 require_once 'PHPUnit/Framework.php';
 require_once dirname(__FILE__) . '../../../../src/rtAutoload.php';
 
+/**
+ * Tests for rtIsExecutableSetTest precondition.
+ *
+ * @category  Testing
+ * @package   RUNTESTS
+ * @author    Zoe Slattery <zoe@php.net>
+ * @author    Stefan Priebsch <spriebsch@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.php.net/license/3_01.txt PHP License 3.01
+ * @link      http://qa.php.net/
+ */
 class rtIsExecutableSetTest extends PHPUnit_Framework_TestCase
 {
     protected function setUp()
@@ -15,14 +37,14 @@ class rtIsExecutableSetTest extends PHPUnit_Framework_TestCase
         unset($this->preCondition);
     }
 
-    public function testClOption()
+    public function testCheckWhenCommandLineOptionIsSet()
     {
         $runtestsConfiguration = rtRuntestsConfiguration::getInstance(array('run-tests.php', '-p', 'some-file'));
         
         $this->assertTrue($this->preCondition->check($runtestsConfiguration));
     }
 
-    public function testEVOption()
+    public function testCheckWhenEnvironmentVariableIsSet()
     {
         $runtestsConfiguration = rtRuntestsConfiguration::getInstance(array('run-tests.php', '-p', 'some-file'));
         $runtestsConfiguration->setEnvironmentVariable('TEST_PHP_EXECUTABLE', 'some-executable');
@@ -30,7 +52,7 @@ class rtIsExecutableSetTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($this->preCondition->check($runtestsConfiguration));
     }
 
-    public function testGetText()
+    public function testGetMessage()
     {
         $this->assertEquals($this->preCondition->getMessage('missingPHPExecutable'), rtText::get('missingPHPExecutable'));
     } 
