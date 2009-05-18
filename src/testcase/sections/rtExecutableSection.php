159f1a31e11c47b0c6de2f7a619b6a8cc061cd5e
Zoe Slattery <zoe@php.net>
2009-05-18 19:27:33 +0000

    typo

--- src/testcase/sections/rtExecutableSection.php
+++ src/testcase/sections/rtExecutableSection.php
@@ -14,7 +14,7 @@ abstract class rtExecutableSection extends rtSection
      
     protected function writeExecutableFile()
     {    
-        // @todo I think \n could be replaced with PHP_EOL here - need to check on Windows.s    
+        // @todo I think \n could be replaced with PHP_EOL here - need to check on Windows.   
         $contentsAsString = implode("\n", $this->sectionContents) . PHP_EOL;
         file_put_contents($this->fileName,  (binary) $contentsAsString);
     }
