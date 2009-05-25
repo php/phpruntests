1d56088ee10a957d61e276fd03badc2ffeac4a68
Zoe Slattery <zoe@php.net>
2009-05-25 12:17:56 +0000

    doc blocs

--- src/testcase/sections/rtConfigurationSection.php
+++ src/testcase/sections/rtConfigurationSection.php
@@ -1,6 +1,17 @@
 <?php
 /**
+ * rtConfigurationSection
+ * 
+ * Parent class for configuration sections
  *
+ *
+ * @category  Testing
+ * @package   RUNTESTS
+ * @author    Zoe Slattery <zoe@php.net>
+ * @author    Stefan Priebsch <spriebsch@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.php.net/license/3_01.txt PHP License 3.01
+ * @link      http://qa.php.net/
  */
 abstract class rtConfigurationSection extends rtSection
 {  
--- src/testcase/sections/rtSection.php
+++ src/testcase/sections/rtSection.php
@@ -1,7 +1,17 @@
 <?php
 /**
- * Parent class for all test case sections
+ * rtSection
+ * 
+ * Parent class for all test sections
  *
+ *
+ * @category  Testing
+ * @package   RUNTESTS
+ * @author    Zoe Slattery <zoe@php.net>
+ * @author    Stefan Priebsch <spriebsch@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.php.net/license/3_01.txt PHP License 3.01
+ * @link      http://qa.php.net/
  */
 abstract class rtSection
 {
