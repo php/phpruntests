fc766c8f7012dbaca871ae9f289f3d9e195eb481
Zoe Slattery <zoe@php.net>
2009-06-02 08:03:32 +0000

    new status class

--- src/rtClassMap.php
+++ src/rtClassMap.php
@@ -1,20 +1,10 @@
 <?php
 /**
- * rtClassMap
- *
  * Autoload Class Map
  *
  * This is a generated file. Do not edit!
  *
  * To re-generate this file, run the script BuildClassMap.php.
- * 
- * @category   Testing
- * @package    RUNTESTS
- * @author     Zoe Slattery <zoe@php.net>
- * @author     Stefan Priebsch <spriebsch@php.net>
- * @copyright  2009 The PHP Group
- * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
- *
  */
 $rtClassMap = array(
     'rtEnvironmentException'                   => 'configuration/exceptions/rtEnvironmentException.php',
@@ -73,6 +63,7 @@ $rtClassMap = array(
     'rtTestOutputWriter'                       => 'testcase/rtTestOutputWriter.php',
     'rtTestPreCondition'                       => 'testcase/rtTestPreCondition.php',
     'rtTestResults'                            => 'testcase/rtTestResults.php',
+    'rtTestStatus'                             => 'testcase/rtTestStatus.php',
     'rtArgsSection'                            => 'testcase/sections/configurationsections/rtArgsSection.php',
     'rtCookieSection'                          => 'testcase/sections/configurationsections/rtCookieSection.php',
     'rtDeflatePostSection'                     => 'testcase/sections/configurationsections/rtDeflatePostSection.php',
