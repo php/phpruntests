c5b7985a911f972b29e9a8b032e96083f9e04166
Zoe Slattery <zoe@php.net>
2009-07-16 16:02:44 +0000

    fixing {PWD} in INI sections

--- tests/testcase/sections/configurationsections/rtIniSectionTest.php
+++ tests/testcase/sections/configurationsections/rtIniSectionTest.php
@@ -7,11 +7,21 @@ class rtIniSectionTest extends PHPUnit_Framework_TestCase
 {
     public function testCreateInstance()
     {
-        $iniSection = rtIniSection::getInstance('INI', array('error_reporting=E_ALL | E_STRICT | E_DEPRECATED', 'assert.active = 1'));  
+        $iniSection = rtIniSection::getInstance('INI', array('error_reporting=E_ALL | E_STRICT | E_DEPRECATED', 'assert.active = 1'));
+        $iniSection->substitutePWD('a-file-name');
         $inilist = $iniSection->getCommandLineArguments();
 
         $this->assertEquals('error_reporting=E_ALL | E_STRICT | E_DEPRECATED', $inilist[0]);
         $this->assertEquals('assert.active = 1', $inilist[1]);
+    }
+    public function testSubtitutePWD()
+    {
+        $iniSection = rtIniSection::getInstance('INI', array('include_path={PWD}'));
+        $afile = __FILE__;
+        $iniSection->substitutePWD($afile);
+        $inilist = $iniSection->getCommandLineArguments();
+
+        $this->assertEquals('include_path=' . dirname($afile), $inilist[0]);
     }
 }
 ?>
