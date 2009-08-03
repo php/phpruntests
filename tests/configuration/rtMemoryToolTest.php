5bd4856cf4ff101c17e1d5143bcb799c975b4bfc
Zoe Slattery <zoe@php.net>
2009-08-03 09:48:53 +0000

    oops

--- tests/configuration/rtMemoryToolTest.php
+++ tests/configuration/rtMemoryToolTest.php
@@ -2,7 +2,7 @@
 require_once 'PHPUnit/Framework.php';
 require_once dirname(__FILE__) . '../../../src/rtAutoload.php';
 
-class rtExternalToolTest extends PHPUnit_Framework_TestCase
+class rtMemoryToolTest extends PHPUnit_Framework_TestCase
 {
 
 
@@ -10,11 +10,11 @@ class rtExternalToolTest extends PHPUnit_Framework_TestCase
     {
 
         $config = rtRuntestsConfiguration::getInstance(array('run-tests.php', '-m', 'test.phpt'));
-        $this->externalTool = rtExternalTool::getInstance($config);
-        $this->externalTool->checkAvailable($config);
-        $this->externalTool->init($config);
+        $this->memoryTool = rtMemoryTool::getInstance($config);
+        $this->memoryTool->checkAvailable($config);
+        $this->memoryTool->init($config);
 
-        $string = substr($this->externalTool->getCommand(), 0, 48);
+        $string = substr($this->memoryTool->getCommand(), 0, 48);
 
         $this->assertEquals('valgrind -q --tool=memcheck --trace-children=yes', $string);
          
@@ -24,11 +24,11 @@ class rtExternalToolTest extends PHPUnit_Framework_TestCase
     {
 
         $config = rtRuntestsConfiguration::getInstance(array('run-tests.php', '-m', '--mopts', '"blah blah"', 'test.phpt'));
-        $this->externalTool = rtExternalTool::getInstance($config);
-        $this->externalTool->checkAvailable($config);
-        $this->externalTool->init($config);
+        $this->memoryTool = rtMemoryTool::getInstance($config);
+        $this->memoryTool->checkAvailable($config);
+        $this->memoryTool->init($config);
 
-        $string = substr($this->externalTool->getCommand(), 0, 58);
+        $string = substr($this->memoryTool->getCommand(), 0, 58);
         $this->assertEquals('valgrind -q --tool=memcheck --trace-children=yes blah blah', $string);
          
     }
